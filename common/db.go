5d353ab32ab1a0afebc513cf71e080b3d9eb92e8
Angie Ramirez <angie@livepeer.org>
2018-12-19 15:42:03 -0500

    db and discovery.go changes, passing tests

--- common/db.go
+++ common/db.go
@@ -19,6 +19,8 @@ type DB struct {
 	dbh *sql.DB
 
 	// prepared statements
+	selectOrchs                *sql.Stmt
+	updateOrchs                *sql.Stmt
 	updateKV                   *sql.Stmt
 	insertJob                  *sql.Stmt
 	selectJobs                 *sql.Stmt
@@ -55,6 +57,11 @@ type DBJob struct {
 	Claims      int64
 }
 
+type DBOrch struct {
+	ServiceURI   string
+	EthereumAddr string
+}
+
 type DBReceipt struct {
 	JobID     int64
 	SeqNo     int64
@@ -83,6 +90,13 @@ var schema = `
 	);
 	INSERT OR IGNORE INTO kv(key, value) VALUES('dbVersion', '{{ . }}');
 	INSERT OR IGNORE INTO kv(key, value) VALUES('lastBlock', '0');
+
+	CREATE TABLE IF NOT EXISTS orchestrators (
+		ethereumAddr STRING PRIMARY KEY,
+		createdAt STRING DEFAULT CURRENT_TIMESTAMP,
+		updatedAt STRING DEFAULT CURRENT_TIMESTAMP,
+		serviceURI STRING
+	);
 
 	CREATE TABLE IF NOT EXISTS jobs (
 		id INTEGER PRIMARY KEY,
@@ -169,6 +183,10 @@ func NewDBJob(id *big.Int, streamID string,
 	}
 }
 
+func NewDBOrch(serviceURI string, orchAddr string) *DBOrch {
+	return &DBOrch{ServiceURI: serviceURI, EthereumAddr: orchAddr}
+}
+
 func DBJobToEthJob(j *DBJob) *lpTypes.Job {
 	return &lpTypes.Job{
 		JobId:               big.NewInt(j.ID),
@@ -228,8 +246,26 @@ func InitDB(dbPath string) (*DB, error) {
 		// all good; nothing to do
 	}
 
+	// select all orchestrators updated in the last 24 hours
+	stmt, err := db.Prepare("SELECT serviceURI, ethereumAddr FROM orchestrators WHERE updatedAt >= datetime('now','-1 day')")
+	if err != nil {
+		glog.Error("Unable to select orchestrators updated in the past 24 hours", err)
+		d.Close()
+		return nil, err
+	}
+	d.selectOrchs = stmt
+
+	// updateOrchestrators statement
+	stmt, err = db.Prepare("INSERT OR REPLACE INTO orchestrators(updatedAt, serviceURI, ethereumAddr) VALUES(datetime(), ?, ?)")
+	if err != nil {
+		glog.Error("Unable to prepare updateOrchestrators stmt ", err)
+		d.Close()
+		return nil, err
+	}
+	d.updateOrchs = stmt
+
 	// updateKV prepared statement
-	stmt, err := db.Prepare("UPDATE kv SET value=?, updatedAt = datetime() WHERE key=?")
+	stmt, err = db.Prepare("UPDATE kv SET value=?, updatedAt = datetime() WHERE key=?")
 	if err != nil {
 		glog.Error("Unable to prepare updatekv stmt ", err)
 		d.Close()
@@ -403,6 +439,12 @@ func InitDB(dbPath string) (*DB, error) {
 
 func (db *DB) Close() {
 	glog.V(DEBUG).Info("Closing DB")
+	if db.updateOrchs != nil {
+		db.updateOrchs.Close()
+	}
+	if db.selectOrchs != nil {
+		db.selectOrchs.Close()
+	}
 	if db.updateKV != nil {
 		db.updateKV.Close()
 	}
@@ -507,6 +549,45 @@ func (db *DB) InsertJob(job *DBJob) error {
 		glog.Error("db: Unable to insert job ", err)
 	}
 	return err
+}
+
+func (db *DB) UpdateOrchs(orch *DBOrch) error {
+	if db == nil {
+		return nil
+	}
+	_, err := db.updateOrchs.Exec(orch.ServiceURI, orch.EthereumAddr)
+	if err != nil {
+		glog.Error("db: Unable to update orchestrator ", err)
+	}
+
+	return err
+}
+
+func (db *DB) SelectOrchs() ([]*DBOrch, error) {
+	if db == nil {
+		return nil, nil
+	}
+
+	rows, err := db.selectOrchs.Query()
+	defer rows.Close()
+	if err != nil {
+		glog.Error("db: Unable to get orchestrators updated in the last 24 hours", err)
+		return nil, err
+	}
+	orchs := []*DBOrch{}
+	for rows.Next() {
+		var orch DBOrch
+		var serviceURI string
+		var ethereumAddr string
+		if err := rows.Scan(&serviceURI, &ethereumAddr); err != nil {
+			glog.Error("db: Unable to fetch orchestrator ", err)
+			continue
+		}
+		orch.ServiceURI = serviceURI
+		orch.EthereumAddr = ethereumAddr
+		orchs = append(orchs, &orch)
+	}
+	return orchs, nil
 }
 
 func (db *DB) ActiveJobs(since *big.Int) ([]*DBJob, error) {
